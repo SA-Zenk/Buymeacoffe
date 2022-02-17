46e9f6d5e48d105690d20d1285fc19e32a08d74e
SA_Zenk <manuelftp1330@gmail.com>
2022-02-17 09:01:33 -0600

    a

---
+++ components/supporter/index.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+	.hero
+		.hero__info
+			p.hero__followers A supporter is worth a thousand followers.
+			p.hero__donation Accept donations. Start a membership. Sell anuthing you like. Its easier than you think
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  padding-left: 24px;
+  padding-right: 24px;
+
+  &__info {
+    font-family: 'Circular Bold';
+  }
+
+  &__followers {
+    line-height: 44px;
+    font-size: 30px;
+    text-align: center;
+  }
+
+  &__donation {
+    font-size: 18px;
+    line-height: 30px;
+    text-align: center;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 div
   Header
+  supporter
   Nuxt
   h1 FOOTER
 </template>
